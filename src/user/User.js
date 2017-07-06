100af5377a8828af6a12bfca6cf585a531093497
Svjat32 <Svjat32@gmail.com>
2017-07-06 18:52:30 +0300

    Add User model with updated spaces

--- src/user/User.js
+++ src/user/User.js
@@ -6,7 +6,7 @@ import mongoose from 'mongoose';
 const Schema = mongoose.Schema;
 
 const UserSchema = new Schema({
-    email: { type: String, unique: true},
-    password: {type: String}
+  email: { type: String, unique: true },
+  password: { type: String },
 });
 const User = mongoose.model('User', UserSchema);
