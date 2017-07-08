299500852a732821cdb10eac215ce653e3f26a21
nikitalpopov <tapochek97@ya.ru>
2017-07-08 14:49:40 +0300

    Add client-side API

---
+++ src/api/api.js
@@ -0,0 +1,18 @@
+/**
+ * Created by @nikitalpopov on 07/07/2017.
+ */
+
+import axios from 'axios';
+
+import { apiPrefix } from '../config.json'
+
+export default {
+    logInUser(data) {
+        return axios.post(`${apiPrefix}/login`, data);
+    },
+
+    loadBoards(data) {
+        return axios.post(`${apiPrefix}/boards`, data);
+    }
+}
+
