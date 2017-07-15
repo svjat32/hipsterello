72128155724281bda4867b1951a831f65a052895
Svjat32 <Svjat32@gmail.com>
2017-07-15 05:08:25 +0300

    CRUD ListAction

--- src/entities/list/ListActions.js
+++ src/entities/list/ListActions.js
@@ -3,11 +3,34 @@
  */
 import api from '../../api/api.js'
 
-export const GET_LIST = 'GET_LIST';
+export const CREATE_LIST = 'CREATE_LIST';
+export const GET_LIST    = 'GET_LIST';
+export const UPDATE_LIST = 'UPDATE_LIST';
+export const DELETE_LIST = 'DELETE_LIST';
+
+export function createList(request) {
+    return {
+        type: CREATE_LIST,
+        payload: api.createList(request)
+        }
+    }
 
 export function getList(request) {
     return {
         type: GET_LIST,
         payload: api.getList(request._id)
+    }
+}
+
+export function updateList(request) {
+    return {
+        type: UPDATE_LIST,
+        payload: api.updateList(request)
+    }
+}
+export function deleteCard(request) {
+    return {
+        type: DELETE_LIST,
+        payload: api.deleteList(request)
     }
 }
